eba0940eabdeb3519825d083eff3b43a9df4ef32
andrew-lewell <andrew.lewell@gmail.com>
2019-12-09 12:20:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  const hourString = time.split(":")[0];
+  const parsedHour = parseInt(hourString);
+  if (parsedHour < 12) {
+    return "Good Morning";
+  } else if (parsedHour > 12 && parsedHour < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  debugger;
+  let greeting = document.querySelector("#greeting");
+  greeting.innerText = `${string}`;
+}
